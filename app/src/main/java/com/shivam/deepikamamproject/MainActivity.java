692182d7ea0b5ab5b1308b2540eea21750a36619
vrishank <vrishank98@gmail.com>
2018-04-13 20:54:40 +0530

    Sorry Bhai

--- app/src/main/java/com/shivam/deepikamamproject/MainActivity.java
+++ app/src/main/java/com/shivam/deepikamamproject/MainActivity.java
@@ -3,22 +3,23 @@ package com.shivam.deepikamamproject;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AlertDialog;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.shivam.deepikamamproject.fragments.AOVD;
 import com.shivam.deepikamamproject.fragments.DiffieHellman;
 import com.shivam.deepikamamproject.fragments.DistanceVector;
 import com.shivam.deepikamamproject.fragments.GoBackN;
+import com.shivam.deepikamamproject.fragments.Home;
 import com.shivam.deepikamamproject.fragments.LinkState;
 import com.shivam.deepikamamproject.fragments.RSA;
 import com.shivam.deepikamamproject.fragments.SelectiveRepeat;
@@ -31,6 +32,9 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        android.app.FragmentManager fragmentManager = getFragmentManager();
+        Home frag = new Home();
+        fragmentManager.beginTransaction().replace(R.id.frame, frag).commit();
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -90,6 +94,9 @@ public class MainActivity extends AppCompatActivity
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            android.app.FragmentManager fragmentManager = getFragmentManager();
+            Home frag = new Home();
+            fragmentManager.beginTransaction().replace(R.id.frame, frag).commit();
             return true;
         }
 
@@ -146,6 +153,12 @@ public class MainActivity extends AppCompatActivity
             fragmentManager.beginTransaction().replace(R.id.frame, frag).commit();
 
         }
+
+        else{
+            android.app.FragmentManager fragmentManager = getFragmentManager();
+        Home frag = new Home();
+        fragmentManager.beginTransaction().replace(R.id.frame, frag).commit();}
+
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
--- app/src/main/java/com/shivam/deepikamamproject/fragments/DistanceVector.java
+++ app/src/main/java/com/shivam/deepikamamproject/fragments/DistanceVector.java
@@ -219,4 +219,86 @@ public class DistanceVector extends android.app.Fragment {
         return  rootview;
     }
 
+    static int graph[][];
+    static int via[][];
+    static int rt[][];
+    static int v;
+    static int e;
+
+    static void dvr_calc_disp(String message)
+    {
+        System.out.println();
+        init_tables();
+        update_tables();
+        System.out.println(message);
+        print_tables();
+        System.out.println();
+    }
+
+    static void update_table(int source)
+    {
+        for(int i = 0; i < v; i++)
+        {
+            if(graph[source][i] != 9999)
+            {
+                int dist = graph[source][i];
+                for(int j = 0; j < v; j++)
+                {
+                    int inter_dist = rt[i][j];
+                    if(via[i][j] == source)
+                        inter_dist = 9999;
+                    if(dist + inter_dist < rt[source][j])
+                    {
+                        rt[source][j] = dist + inter_dist;
+                        via[source][j] = i;
+                    }
+                }
+            }
+        }
+    }
+
+    static void update_tables()
+    {
+        int k = 0;
+        for(int i = 0; i < 4*v; i++)
+        {
+            update_table(k);
+            k++;
+            if(k == v)
+                k = 0;
+        }
+    }
+
+    static void init_tables()
+    {
+        for(int i = 0; i < v; i++)
+        {
+            for(int j = 0; j < v; j++)
+            {
+                if(i == j)
+                {
+                    rt[i][j] = 0;
+                    via[i][j] = i;
+                }
+                else
+                {
+                    rt[i][j] = 9999;
+                    via[i][j] = 100;
+                }
+            }
+        }
+    }
+
+    static void print_tables()
+    {
+        for(int i = 0; i < v; i++)
+        {
+            for(int j = 0; j < v; j++)
+            {
+                System.out.print("Dist: " + rt[i][j] + "    ");
+            }
+            System.out.println();
+        }
+    }
+
 }
